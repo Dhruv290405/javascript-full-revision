d05067231efd558b2b869dd97fcbd3938f3a5c98
Dhruv Tiwari <164215527+Dhruv290405@users.noreply.github.com>
2025-03-20 07:14:20 +0000

    container config done

---
+++ basics /var.js
@@ -0,0 +1,13 @@
+const accountId =12334
+let accountEmail = "dhruvv@gmail"
+var accountPassword = "djr="
+accountCity ="ujjain"
+let accountState;
+
+// accountId =2   not allowed
+
+accountEmail = "h@gmail.com"
+accountPassword = "23321"
+accountCity ="taj mahal"
+console.log(accountId);
+console.table([accountId , accountEmail, accountPassword , accountCity, accountState])
