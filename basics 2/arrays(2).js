81a90f6ea752245358542944f6d2ef031a2dd5b3
Dhruv Tiwari <164215527+Dhruv290405@users.noreply.github.com>
2025-04-02 04:48:24 +0000

    more about arrays in js

--- basics 2/arrays(2).js
+++ basics 2/arrays(2).js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("dhruv"))
+console.log(Array.from("dhruv"))
+console.log(Array.from({name: "dhruv"})) // interesting
 
 let score1 = 100
 let score2 = 200
